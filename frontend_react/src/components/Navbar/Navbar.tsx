c2e3d2e2635c98598dbcd6dc72f85084571a38e5
Jason Wu <jiangwu0228@gmail.com>
2022-03-08 22:50:56 +1300

    navbar side

--- frontend_react/src/components/Navbar/Navbar.tsx
+++ frontend_react/src/components/Navbar/Navbar.tsx
@@ -1,8 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Navbar.scss";
 import { images } from "../../constants";
+import { HiMenuAlt4, HiX } from "react-icons/hi";
+import { motion } from "framer-motion";
 
 const Navbar = () => {
+  const [toggle, setToggle] = useState(false);
   const menus: string[] = ["Home", "About", "Works", "Skills", "Contact"];
   return (
     <nav className="app__navbar">
@@ -17,6 +20,27 @@ const Navbar = () => {
           </li>
         ))}
       </ul>
+
+      <div className="app__navbar-menu">
+        <HiMenuAlt4 onClick={() => setToggle(true)} />
+        {toggle && (
+          <motion.div
+            whileInView={{ x: [300, 0] }}
+            transition={{ duration: 0.85, ease: "easeOut" }}
+          >
+            <HiX onClick={() => setToggle(false)} />
+            <ul>
+              {menus.map((item) => (
+                <li key={item}>
+                  <a href={`#${item}`} onClick={() => setToggle(false)}>
+                    {item}
+                  </a>
+                </li>
+              ))}
+            </ul>
+          </motion.div>
+        )}
+      </div>
     </nav>
   );
 };
