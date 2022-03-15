431e834e3648c382165c13321c2e911ebc6107df
Jason Wu <jiangwu0228@gmail.com>
2022-03-15 17:35:20 +1300

    header done

--- frontend_react/src/container/Header/Header.tsx
+++ frontend_react/src/container/Header/Header.tsx
@@ -1,10 +1,71 @@
-import React from 'react'
-import './Header.scss'
+import React from "react";
+import { motion } from "framer-motion";
+import { images } from "../../constants";
+import "./Header.scss";
+
+const scaleVariants = {
+  whileInView: {
+    scale: [0, 1],
+    opacity: [0, 1],
+    transition: {
+      duration: 1,
+      ease: "easeInOut",
+    },
+  },
+};
+
+const techImg = [images.flutter, images.redux, images.sass]
 
 function Header() {
   return (
-    <div>Header</div>
-  )
+    <div id="home" className="app__header app__flex">
+      <motion.div
+        className="app__header-info"
+        whileInView={{ x: [-100, 0], opacity: [0, 1] }}
+        transition={{ duration: 0.85, ease: "easeInOut" }}
+      >
+        <div className="app__header-badge">
+          <div className="badge-cmp app__flex">
+            <span>👋</span>
+            <div style={{ marginLeft: 20 }}>
+              <p className="p-text">Hello, I am</p>
+              <h1 className="head-text">Jason</h1>
+            </div>
+          </div>
+          <div className="tag-cmp app__flex">
+            <p className="p-text">Web Developer</p>
+            <p className="p-text">Freelancer</p>
+          </div>
+        </div>
+      </motion.div>
+
+      <motion.div
+        className="app__header-img"
+        whileInView={{ opacity: [0, 1] }}
+        transition={{ duration: 0.5, delayChildren: 0.5, ease: "easeInOut" }}
+      >
+        <img src={images.profile} alt="profile_bg" style={{height: 710, width: 425}}/>
+        <motion.img
+          whileInView={{ scale: [0, 1] }}
+          transition={{ duration: 1, ease: "easeInOut" }}
+          src={images.circle}
+          alt="profile_circle"
+          className="overlay_circle"
+        />
+      </motion.div>
+      <motion.div
+        variants={scaleVariants}
+        whileInView={scaleVariants.whileInView}
+        className="app__header-circles"
+      >
+        {techImg.map((circle,index) => (
+          <div className="circle-cmp app__flex" key={`circle-${index}`}>
+            <img src={circle}  alt="profile_bg" />
+          </div>
+        ))}
+      </motion.div>
+    </div>
+  );
 }
 
-export default Header
+export default Header;
