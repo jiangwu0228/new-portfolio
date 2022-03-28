bd40016792185bb9fc215b308e702cbfcc9de681
Jason Wu <jiangwu0228@gmail.com>
2022-03-28 22:45:38 +1300

    update works

--- frontend_react/src/container/Works/Works.tsx
+++ frontend_react/src/container/Works/Works.tsx
@@ -13,31 +13,108 @@ const workCategory: string[] = [
 ];
 
 function Works() {
-  const [worksData, setWorksData] = useState<string[]>([]);
-  const [filterWork, setFilterWork] = useState<string[]>([]);
+  const [works, setWorks] = useState<any[]>([]);
+  // const [workCategory, setWorkCategory] = useState<string[]>([]);
+  const [filterWork, setFilterWork] = useState<any[]>([]);
   const [activeCategory, setActiveCategory] = useState<string>("All");
+  const [animateCard, setAnimateCard] = useState({ y: 0, opacity: 1 });
+
+  useEffect(() => {
+    const query =
+      '*[_type == "works"]{id, title, shortDesc, situation, task, actions, result, "imgUrl": imgUrl.asset->url, "icon": icon.asset->url, codeLink, tag, techs}';
+
+    client.fetch(query).then((data) => {
+      setWorks(data);
+      setFilterWork(data);
+    });
+  }, []);
+
+  // useEffect(() => {
+  //   const query = '*[_type == "workCategory"]';
+  //   client.fetch(query).then((data) => {
+  //     setWorkCategory(data);
+  //   });
+  // }, []);
 
   const handleWorkFilter = (category: string) => {
     setActiveCategory(category);
   };
   return (
     <>
+      <div className="app_works">
+
+      
       <h2 className="head-text">
         My Creative <span>Portfolio</span> <br /> means{" "}
         <span>Good Business</span>
       </h2>
-      <div className="app__work-filter">
+      <div className="app__works-filter">
         {workCategory.map((category, index) => {
           return (
             <div
               key={index}
-              onClick={() => {}}
-              className={`app__work-filter-item app__flex p-text ${
+              onClick={() => handleWorkFilter(category)}
+              className={`app__works-filter-item app__flex p-text ${
                 activeCategory === category ? "active" : ""
               }`}
-            >{category}</div>
+            >
+              {category}
+            </div>
           );
         })}
+      </div>
+      <motion.div
+        animate={animateCard}
+        transition={{ duration: 0.5, delayChildren: 0.5 }}
+        className="app__work-portfolio"
+      >
+        {filterWork.map((work, index) => {
+          return (
+            <div className="app__work-item app__flex" key={index}>
+              <div className="app__work-img app__flex">
+                <img src={work.imgUrl} alt={work.title} />
+
+                <motion.div
+                  whileHover={{ opacity: [0, 1] }}
+                  transition={{
+                    duration: 0.25,
+                    ease: "easeInOut",
+                    staggerChildren: 0.5,
+                  }}
+                  className="app__work-hover app__flex"
+                >
+                  <a href={work.codeLink} target="_blank" rel="noreferrer">
+                    <motion.div
+                      whileInView={{ scale: [0, 1] }}
+                      whileHover={{ scale: [1, 0.9] }}
+                      transition={{
+                        duration: 0.25,
+                        ease: "easeInOut",
+                        staggerChildren: 0.5,
+                      }}
+                      className="app__work-hover app__flex"
+                    >
+                      <AiFillEye />
+                    </motion.div>
+                  </a>
+                </motion.div>
+              </div>
+              <div className="app__work-content app__flex">
+                <h4 className="bold-text">{work.title}</h4>
+                <p className="p-text" style={{ marginTop: 10 }}>
+                  {work.shortDesc}
+                </p>
+
+                <div className="app__work-tag app__flex">
+                  {work.techs.map((tech: string, index:number) => {
+                    return <p className="p-text" key={index}>{tech}</p>;
+                  })}
+                </div>
+              </div>
+            </div>
+          );
+        })}
+      </motion.div>
       </div>
     </>
   );
