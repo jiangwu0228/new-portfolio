4c2298bb0f28158bfbe41b700478e9835c366d65
Jason Wu <jiangwu0228@gmail.com>
2022-04-30 23:44:42 +1200

    banckend workcatgory update

--- backend_sanity/schemas/portfolio/workCategory.js
+++ backend_sanity/schemas/portfolio/workCategory.js
@@ -3,7 +3,11 @@ export default {
   title: "Work Category",
   type: "document",
   fields: [
-    { name: "id", title: "Id", type: "string" },
+    {
+      name: "id",
+      title: "Id",
+      type: "string",
+    },
     {
       name: "title",
       title: "Title",
